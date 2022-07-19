0bd060f0d71c536f877598aaef014eddd6020d44
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-19 09:24:15 +0300

    Removed the error message from the password field

--- Models/UserLoginRequest.cs
+++ Models/UserLoginRequest.cs
@@ -6,6 +6,6 @@ public class UserLoginRequest
 {
     [Required, EmailAddress]
     public string Email { get; set; } = string.Empty;
-    [Required, MinLength(6, ErrorMessage = "You should enter at leat 6 characters")]
+    [Required, MinLength(6)]
     public string Password { get; set; } = string.Empty;
 }
