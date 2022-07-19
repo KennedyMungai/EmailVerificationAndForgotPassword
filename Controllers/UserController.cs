0613cde0b59a0a6ac733233f78633982eba572cc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-19 09:10:01 +0300

    Created a Registration function

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmailVerificationAndForgotPassword.Controllers;
@@ -11,5 +12,43 @@ public class UserController : ControllerBase
     public UserController(DataContext context)
     {
         this.context = context;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(UserRegisterRequest request)
+    {
+        if(context.Users.Any(u => u.Email == request.Email))
+        {
+            return BadRequest("User already exists");
+        }
+
+        CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+        var user = new User
+        {
+            Email = request.Email,
+            PasswordHash = passwordHash,
+            PasswordSalt = passwordSalt,
+            VerificationToken = CreateRandomToken()
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return Ok("User successfully created");
+    }
+
+    private string CreateRandomToken()
+    {
+        return Convert.ToHexString(RandomNumberGenerator.GetBytes(64));
+    }
+
+    private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+        using(var hmac = new HMACSHA512())
+        {
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
     }
 }
