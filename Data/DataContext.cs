6fee796a107c666b6318f637640abb0d096578ac
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-19 08:13:09 +0300

    Added the DB connection string

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -7,11 +7,14 @@ namespace EmailVerificationAndForgotPassword.Data;
 public class DataContext : DbContext
 {
     public DbSet<User>? Users { get; set; }
-    
-    
 
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
         
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS; Database=SomeRandomName; Trusted_Connection=true; MultipleActiveResultSets=true;");
     }
 }
