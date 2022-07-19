9fd6de80080df7dfb2a7593214a5439d7c938a93
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-19 09:22:23 +0300

    Added an error messgae for the Password Length

--- Models/UserRegisterRequest.cs
+++ Models/UserRegisterRequest.cs
@@ -6,7 +6,7 @@ public class UserRegisterRequest
 {
     [Required, EmailAddress]
     public string Email { get; set; } = string.Empty;
-    [Required, MinLength(6)]
+    [Required, MinLength(6, ErrorMessage = "You should enter at leat 6 characters")]
     public string Password { get; set; } = string.Empty;
     [Required, Compare("Password")]
     public string ConfirmPassword { get; set; } = string.Empty;
